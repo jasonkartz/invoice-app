5b84fdfd57fb5486e2ec3b15098148308ba2798e
Jason Kartz <jasonkartz@gmail.com>
2022-10-15 12:13:42 -0500

    invoice preview status colors

--- src/components/invoices/InvoicePreview.js
+++ src/components/invoices/InvoicePreview.js
@@ -7,16 +7,25 @@ export default function InvoicePreview({
   total,
   status,
 }) {
+  const statusColor = () => {
+    if (status === "paid") {
+      return styles.statusPaid;
+    } else if (status === "pending") {
+      return styles.statusPending;
+    } else {
+      return styles.statusDraft;
+    }
+  };
   return (
     <div className={styles.container}>
       <h4 className={styles.id}>
-        <span>#</span>
+        <span className={styles.pound}>#</span>
         {id}
       </h4>
       <span className={styles.paymentDue}>Due {paymentDue}</span>
       <span className={styles.clientName}>{clientName}</span>
-      <h3 className={styles.total}>${total}</h3>
-      <h4 className={styles.status}>
+      <h3 className={styles.total}>${total.toFixed(2)}</h3>
+      <h4 className={`${styles.status} ${statusColor()}`}>
         <span className={styles.bullet}>&middot; </span>
         <span>{status}</span>
       </h4>
